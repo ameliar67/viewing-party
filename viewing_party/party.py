92ca64474f04ef4dfd0ad120bed7c0af5d38725b
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2026-09-16 16:45:57 -0700

    Wave 2 tests pass and functions completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,23 +10,59 @@ def create_movie(title, genre, rating):
         "rating": rating
     }
 
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
-
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
 
     return user_data
 
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
-
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
 
     return user_data
+
+# -----------------------------------------
+# ------------- WAVE 2 --------------------
+# -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+
+    total_rating = 0.0
+
+    watched_movies = user_data['watched']
+    if len(watched_movies) == 0:
+        return 0.0
+
+    for movie in watched_movies:
+        total_rating+=movie["rating"]
+
+    return total_rating / len(user_data['watched'])
+
+def get_most_watched_genre(user_data):
+
+    genres = {}
+    current_highest_genre = ''
+    current_highest_genre_count = 0
+
+    if len(user_data["watched"]) == 0:
+        return None
+
+    for movie in user_data["watched"]:
+        if movie['genre'] in genres:
+            genres[movie['genre']]+=1
+        else:
+            genres[movie['genre']] = 1
+
+    for genre in genres.keys():
+        if genres[genre] > current_highest_genre_count:
+            current_highest_genre_count = genres[genre]
+            current_highest_genre = genre
+
+    return current_highest_genre 
+
+# -----------------------------------------
+# ------------- WAVE 3 --------------------
+# -----------------------------------------
+
 
         
 # -----------------------------------------
